4498386bd2fa3fe985ac392a5d5d129b267639a3
liu-yihong <lfa_1@126.com>
2023-04-18 17:53:51 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,15 @@ from ics.grammar.parse import ContentLine
 from datetime import datetime as dt
 from dateutil import tz
 import pandas as pd
+
+if __name__ == '__main__':
+    c = Calendar()
+    e = Event()
+    e.name = 'Test'
+    e.begin = dt(2019, 1, 1, 0, 0, 0, 0, tz.tzlocal())
+    e.end = dt(2019, 1, 1, 0, 0, 0, 0, tz.tzlocal())
+    c.events.add(e)
+    print(c.events)
+    
+    with open('MISConferenceCalendar.ics', 'w') as my_file:
+    my_file.write(str(c))
